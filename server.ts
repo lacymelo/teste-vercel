ff20c57b97e12576feeaf60a141ca08a6bc80ae2
lacy <lacienemelo@MacBook-Air-de-Laciene.local>
2022-12-14 21:43:43 -0300

    teste com vercel 0.3

--- server.ts
+++ server.ts
@@ -1,6 +1,10 @@
-import express from "express"
+import express, { request, response } from "express"
 
 const app = express()
+
+app.get('/', (request, response) =>
+    response.json({ Ok: 'Entrou na rota' })
+)
 
 //iniciando o servidor
 app.listen(3333, () => console.log("Server is running!"))
